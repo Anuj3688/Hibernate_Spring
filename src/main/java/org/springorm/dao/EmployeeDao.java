ce852edd38d58dc6a5b9bc195d4885807b8f1041
Anuj Tiwari <anuj.10tiwari2001@gmail.com>
2024-05-27 23:28:29 +0530

    last Error reached not able to figure out how Employee is showing un known

--- src/main/java/org/springorm/dao/EmployeeDao.java
+++ src/main/java/org/springorm/dao/EmployeeDao.java
@@ -1,15 +1,25 @@
 package org.springorm.dao;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springorm.entities.Employee;
+
+import javax.transaction.Transactional;
 
 public class EmployeeDao {
     private HibernateTemplate hibernateTemplate;
 
+    @Transactional
     public int insert(Employee employee){
         //Inserting using hibernateTemplate
         Integer i = (Integer) this.hibernateTemplate.save(employee);
         return i;
+    }
+
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
     }
 }
